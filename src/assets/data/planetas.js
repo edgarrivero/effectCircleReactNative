668f7f0d31c963b7f1caa50f196766d1d1304027
Edgar Rivero <riveroedgar.jm@gmail.com>
2024-06-16 13:25:24 -0500

    New plantes

--- src/assets/data/planetas.js
+++ src/assets/data/planetas.js
@@ -1,7 +1,7 @@
 export default [
   {
     nombre: "Mercury",
-    imagen: "https://raw.githubusercontent.com/edgarrivero/assets/main/planeta1.png",
+    imagen: "https://raw.githubusercontent.com/edgarrivero/assets/main/planet1-01.png",
     nivel: "Beginner",
     descripcion: "Mercury is the closest planet to the Sun.",
     alignItems: 'flex-start', // Alineación horizontal
@@ -9,10 +9,11 @@ export default [
     with: 150,
     height: 100,
     navigateTo: "Level1",
+    paddingLeft: 100
   },
   {
     nombre: "Venus",
-    imagen: "https://raw.githubusercontent.com/edgarrivero/assets/main/planeta2.png",
+    imagen: "https://raw.githubusercontent.com/edgarrivero/assets/main/planet2-01.png",
     nivel: "Beginner",
     descripcion: "Venus is known for its thick atmosphere.",
     alignItems: 'flex-end',
@@ -20,10 +21,83 @@ export default [
     with: 70,
     height: 70,
     navigateTo: "Level2",
+    paddingLeft: 10
   },
   {
     nombre: "Earth",
-    imagen: "https://raw.githubusercontent.com/edgarrivero/assets/main/planeta3.png",
+    imagen: "https://raw.githubusercontent.com/edgarrivero/assets/main/planet3-01.png",
+    nivel: "Beginner",
+    descripcion: "Earth is the third planet from the Sun and the only one known to support life.",
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    with: 90,
+    height: 80,
+    navigateTo: "Level3",
+    paddingLeft: 100
+  },
+  {
+    nombre: "Mars",
+    imagen: "https://raw.githubusercontent.com/edgarrivero/assets/main/planet4-01.png",
+    nivel: "Intermediate",
+    descripcion: "Mars is often called the Red Planet.",
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+    with: 150,
+    height: 150,
+    navigateTo: "Level4",
+    paddingLeft: 200
+  },
+  {
+    nombre: "Mercury",
+    imagen: "https://raw.githubusercontent.com/edgarrivero/assets/main/planet5-01.png",
+    nivel: "Beginner",
+    descripcion: "Mercury is the closest planet to the Sun.",
+    alignItems: 'flex-start', // Alineación horizontal
+    justifyContent: 'flex-start', // Alineación vertical
+    with: 80,
+    height: 100,
+    navigateTo: "Level5",
+    paddingLeft: 40
+  },
+  {
+    nombre: "Venus",
+    imagen: "https://raw.githubusercontent.com/edgarrivero/assets/main/planet6-01.png",
+    nivel: "Beginner",
+    descripcion: "Venus is known for its thick atmosphere.",
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+    with: 180,
+    height: 90,
+    navigateTo: "Level6",
+    paddingLeft: 30
+  },
+  {
+    nombre: "Mercury",
+    imagen: "https://raw.githubusercontent.com/edgarrivero/assets/main/planet7-01.png",
+    nivel: "Beginner",
+    descripcion: "Mercury is the closest planet to the Sun.",
+    alignItems: 'flex-start', // Alineación horizontal
+    justifyContent: 'flex-start', // Alineación vertical
+    with: 150,
+    height: 100,
+    navigateTo: "Level1",
+    paddingLeft: 80
+  },
+  {
+    nombre: "Venus",
+    imagen: "https://raw.githubusercontent.com/edgarrivero/assets/main/planet8-01.png",
+    nivel: "Beginner",
+    descripcion: "Venus is known for its thick atmosphere.",
+    alignItems: 'flex-end',
+    justifyContent: 'flex-start',
+    with: 70,
+    height: 70,
+    navigateTo: "Level2",
+    paddingLeft: 90
+  },
+  {
+    nombre: "Earth",
+    imagen: "https://raw.githubusercontent.com/edgarrivero/assets/main/planet9-01.png",
     nivel: "Beginner",
     descripcion: "Earth is the third planet from the Sun and the only one known to support life.",
     alignItems: 'flex-start',
@@ -31,6 +105,7 @@ export default [
     with: 50,
     height: 80,
     navigateTo: "Level3",
+    paddingLeft: 120
   },
   {
     nombre: "Mars",
@@ -42,6 +117,7 @@ export default [
     with: 150,
     height: 150,
     navigateTo: "Level4",
+    paddingLeft: 140
   },
   {
     nombre: "Mercury",
@@ -53,6 +129,7 @@ export default [
     with: 80,
     height: 100,
     navigateTo: "Level5",
+    paddingLeft: 160
   },
   {
     nombre: "Venus",
@@ -64,72 +141,7 @@ export default [
     with: 180,
     height: 90,
     navigateTo: "Level6",
-  },
-  {
-    nombre: "Mercury",
-    imagen: "https://raw.githubusercontent.com/edgarrivero/assets/main/planeta1.png",
-    nivel: "Beginner",
-    descripcion: "Mercury is the closest planet to the Sun.",
-    alignItems: 'flex-start', // Alineación horizontal
-    justifyContent: 'flex-start', // Alineación vertical
-    with: 150,
-    height: 100,
-    navigateTo: "Level1",
-  },
-  {
-    nombre: "Venus",
-    imagen: "https://raw.githubusercontent.com/edgarrivero/assets/main/planeta2.png",
-    nivel: "Beginner",
-    descripcion: "Venus is known for its thick atmosphere.",
-    alignItems: 'flex-end',
-    justifyContent: 'flex-start',
-    with: 70,
-    height: 70,
-    navigateTo: "Level2",
-  },
-  {
-    nombre: "Earth",
-    imagen: "https://raw.githubusercontent.com/edgarrivero/assets/main/planeta3.png",
-    nivel: "Beginner",
-    descripcion: "Earth is the third planet from the Sun and the only one known to support life.",
-    alignItems: 'flex-start',
-    justifyContent: 'center',
-    with: 50,
-    height: 80,
-    navigateTo: "Level3",
-  },
-  {
-    nombre: "Mars",
-    imagen: "https://raw.githubusercontent.com/edgarrivero/assets/main/planeta4.png",
-    nivel: "Intermediate",
-    descripcion: "Mars is often called the Red Planet.",
-    alignItems: 'flex-end',
-    justifyContent: 'center',
-    with: 150,
-    height: 150,
-    navigateTo: "Level4",
-  },
-  {
-    nombre: "Mercury",
-    imagen: "https://raw.githubusercontent.com/edgarrivero/assets/main/planeta5.png",
-    nivel: "Beginner",
-    descripcion: "Mercury is the closest planet to the Sun.",
-    alignItems: 'flex-start', // Alineación horizontal
-    justifyContent: 'flex-start', // Alineación vertical
-    with: 80,
-    height: 100,
-    navigateTo: "Level5",
-  },
-  {
-    nombre: "Venus",
-    imagen: "https://raw.githubusercontent.com/edgarrivero/assets/main/planeta6.png",
-    nivel: "Beginner",
-    descripcion: "Venus is known for its thick atmosphere.",
-    alignItems: 'flex-end',
-    justifyContent: 'center',
-    with: 180,
-    height: 90,
-    navigateTo: "Level6",
+    paddingLeft: 180
   },
   
 ]
